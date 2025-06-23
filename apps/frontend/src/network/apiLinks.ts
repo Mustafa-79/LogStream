1185a9508075cd026a2a6ee61d3af2231af1e266
AliSaif541 <muhammadalisaif2@gmail.com>
2025-06-23 14:58:22 +0500

    Frontend Repo Setup

---
+++ apps/frontend/src/network/apiLinks.ts
@@ -0,0 +1,9 @@
+class ApiLinks {
+  static API_BASE_URL: string = import.meta.env.VITE_SERVER_URL as string;
+
+  // User Login
+//   static LOGIN_URL: string = '/auth/login';
+
+}
+
+export default ApiLinks;
---
+++ apps/frontend/src/routes/index.tsx
@@ -0,0 +1,61 @@
+import type { RouteObject } from 'react-router-dom';
+import type { ReactElement } from 'react';
+import { useRoutes } from 'react-router-dom';
+
+import DashboardLayout from '../layout/DashboardLayout';
+import Analytics from '../pages/Analytics';
+import Dashboard from '../pages/Dashboard';
+import Applications from '../pages/Applications';
+import UserGroups from '../pages/UserGroups';
+import Settings from '../pages/Settings';
+import NotFound from '../pages/NotFound';
+
+// Type-safe route array
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: 'dashboard',
+        element: (
+              <Dashboard />
+        ),
+      },
+      {
+        path: 'analytics',
+        element: (
+            <Analytics />
+        ),
+      },
+      {
+        path: 'settings',
+        element: (
+          <Settings />
+        ),
+      },
+      {
+        path: 'applications',
+        element: (
+          <Applications />
+        ),
+      },
+      {
+        path: 'user-groups',
+        element: (
+          <UserGroups />
+        ),
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: (
+        <NotFound />
+    ),
+  },
+];
+
+export default function ThemeRoutes(): ReactElement | null {
+  return useRoutes(routes);
+}
