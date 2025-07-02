e0e7bb159d9af2a4906799aee29e7d813b272c47
AliSaif541 <muhammadalisaif2@gmail.com>
2025-07-02 16:08:51 +0500

    Application Validations

--- apps/api-gateway/src/validations/applicationValidations.ts
+++ apps/api-gateway/src/validations/applicationValidations.ts
@@ -3,28 +3,62 @@ import Joi from 'joi';
 export const applicationValidation = {
   createApplication: {
     body: Joi.object({
-      name: Joi.string().required().label('Application Name'),
-      description: Joi.string().required().label('Application Description'),
+      name: Joi.string()
+        .min(2)
+        .max(50)
+        .required()
+        .label('Application Name'),
+      description: Joi.string()
+        .min(5)
+        .max(1000)
+        .required()
+        .label('Application Description'),
     }),
   },
+
   updateApplication: {
     params: Joi.object({
-      id: Joi.string().required().length(24).label('Application ID'), 
+      id: Joi.string()
+        .length(24)
+        .required()
+        .label('Application ID'), 
     }),
     body: Joi.object({
-      name: Joi.string().optional().label('Application Name'),
-      description: Joi.string().optional().label('Application Description'),
+      name: Joi.string()
+        .min(2)
+        .max(50)
+        .optional()
+        .label('Application Name'),
+      description: Joi.string()
+        .min(5)
+        .max(1000)
+        .optional()
+        .label('Application Description'),
+      active: Joi.boolean()
+        .optional()
+        .label('Application Active Status'),
     }).min(1),
   },
+
   deleteApplication: {
     params: Joi.object({
-      id: Joi.string().required().length(24).label('Application ID'), 
+      id: Joi.string()
+        .length(24)
+        .required()
+        .label('Application ID'), 
     }),
   },
+
   updateThresholdAndTimePeriod: {
     body: Joi.object({
-      threshold: Joi.number().required().label('Threshold'),
-      time_period: Joi.number().required().label('Time Period'),
+      threshold: Joi.number()
+        .greater(0)
+        .required()
+        .label('Threshold'),
+      time_period: Joi.number()
+        .greater(0)
+        .required()
+        .label('Time Period'),
     }),
   },
 };
