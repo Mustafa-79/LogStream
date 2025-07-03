dbd95ae18bd17dc513ac08b9e0776313043d22b1
Muhammad Ammar Shahzad <ammarshahzad365@gmail.com>
2025-07-03 12:34:52 +0500

    removed public routes

--- apps/api-gateway/src/middlewares/auth.ts
+++ apps/api-gateway/src/middlewares/auth.ts
@@ -67,30 +67,3 @@ export const requireAdmin = (req: Request, res: Response, next: NextFunction): v
 
   next();
 };
-
-/**
- * Optional JWT Authentication Middleware
- * Adds user data to request if token is present, but doesn't fail if missing
- */
-export const optionalAuth = (req: Request, res: Response, next: NextFunction): void => {
-  const authHeader = req.headers.authorization;
-  
-  if (!authHeader) {
-    return next();
-  }
-
-  const token = authHeader.split(' ')[1];
-  
-  if (!token) {
-    return next();
-  }
-
-  try {
-    const decoded = jwt.verify(token, config.jwtSecret) as UserPayload;
-    req.user = decoded;
-  } catch {
-    // Silently ignore token errors for optional auth
-  }
-  
-  next();
-};
