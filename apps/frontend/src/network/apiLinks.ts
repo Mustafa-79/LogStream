94247f76bcf40af0a41038272eb54f2a354a5f8d
mustafa-abbas <mustafa.abbas@gosaas.io>
2025-07-18 11:51:54 +0500

    removed unnecessary files

--- apps/frontend/src/network/apiLinks.ts
+++
@@ -1,9 +0,0 @@
-class ApiLinks {
-  static API_BASE_URL: string = import.meta.env.VITE_SERVER_URL as string;
-
-  // User Login
-//   static LOGIN_URL: string = '/auth/login';
-
-}
-
-export default ApiLinks;
--- apps/frontend/src/pages/UserGroups.tsx
+++
@@ -1,9 +0,0 @@
-const UserGroups = () => {
-    return (
-        <>
-        <h1>User Groups</h1>
-        </>
-    )
-}
-
-export default UserGroups
